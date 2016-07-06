a75bd8b627b840016d2ef9e5f51c19d47a367feb
Dmitriy Volchenko <dima201246@gmail.com>
2016-07-07 00:48:18 +0300

    Библиотека для работы с конфигурационными файлами

---
+++ configurator/configurator.h
@@ -0,0 +1,16 @@
+#ifndef CONFIGURATOR_H
+#define CONFIGURATOR_H
+
+	#include <fstream>
+	#include <vector>
+	#include <string>
+	#include <stdlib.h>
+
+	extern	std::vector <std::string>	main_config_base;
+
+	std::string configurator(std::string /*Путь к файлу*/, std::string /*Искомый параметр*/, std::string /*Новое значение, если нужна перезапись*/, bool /*Перезапись значения или нет*/); // Поиск в файле нужного параметра и возврат его значения
+	void add_to_file(std::string /*Путь к файлу*/, std::string /*Строка, которая будет добавлена*/); // Запись в конец файла
+	std::string conf(std::string /*Искомый параметр*/, std::vector<std::string> /*вектор со значениями*/); //Поиск в векторе нужного параметра и возврат его значения
+	bool load_to_vector(std::string /*Имя файла*/, std::vector<std::string>&  /*Ссылка на вектор*/); // Загрузка файла в вектор
+
+#endif
