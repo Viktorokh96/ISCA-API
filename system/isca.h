1a63cd92323ec703676306019754d8dd11a1bb60
Dmitriy Volchenko <dima201246@gmail.com>
2016-07-07 01:04:05 +0300

    Для использования либы

---
+++ system/isca.h
@@ -0,0 +1,14 @@
+/*
+|	ISCA API
+|
+|	Authors:	Viktor Ohotnikov  (viktorokh96@mail.ru)
+|				Dmitriy Volchenko (dima201246@gmail.com)
+|
+*/
+
+#include "../configurator/configurator.h"
+#include "../fswork/fswork.h"
+#include "../lang/lang.h"
+#include "../screen/screen.h"
+#include "../time/time.h"
+#include "../windlg/windlg.h"
