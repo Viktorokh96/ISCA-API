31954d5589ded8b2729fe5b3d59e27e89f7728ab
Viktorokh96 <viktorokh96@mail.ru>
2016-07-10 15:59:19 +0300

    обдумывание каркаса

--- testing/framework.cpp
+++ testing/framework.cpp
@@ -1,119 +1,18 @@
 #include <iostream>
 #include <string>
 #include <unistd.h>
-#include <cstdarg>
-#include <sstream>
 #include <cstdlib>
 #include "../iscalib/system/isca.h"
+#include "./iosystem.h"
 
-/*################ IO SYSTEM #########################*/
+struct ISCA_Win {
 
-template <class T>
-std::string convertNumber(T num)
+};
+
+int ISCA_RegisterWindow(ISCA_Win win)
 {
-    std::stringstream ss;   // <-- also note namespace qualification
-    ss << num;
-    return ss.str();
+	return 0;
 }
-
-void ISCA_Log(std::ostream &out, std::string msg, ...)
-{
-	va_list p;
-	va_start(p, msg);
-
-	std::string buff = "ISCA_LOG:";
-	const char *ch = msg.c_str();
-
-	while(*ch) {
-		if(*ch == '%') {
-			ch++;
-			switch(*ch) {
-				case 'i':
-				case 'd':
-				case 'u':
-						buff += convertNumber(va_arg(p, int));
-					break;
-
-				case 'e':
-				case 'f':
-				case 'g':
-						buff += convertNumber(va_arg(p, double));
-					break;
-
-				case 's':
-						buff += va_arg(p, char *);
-					break;
-
-				case 'c':
-						buff.push_back(va_arg(p, int));
-					break;
-			}
-			ch++;
-		}
-
-		buff.push_back(*ch);
-		ch++;
-	}
-
-	out << buff;
-}
-
-void ISCA_Log(std::string filename, std::string msg, ...)
-{
-	std::ofstream out;
-	out.open(filename.c_str(), 
-		std::ios::out | std::ios::app);
-
-	if(!out) {
-		std::cerr << "Не удается открыть файл для ISCA_LOG." 
-			<< std::endl;
-		return;
-	}
-
-	va_list p;
-	va_start(p, msg);
-
-	std::string buff = "ISCA_LOG:";
-	const char *ch = msg.c_str();
-
-	while(*ch) {
-		if(*ch == '%') {
-			ch++;
-			switch(*ch) {
-				case 'i':
-				case 'd':
-				case 'u':
-						buff += convertNumber(va_arg(p, int));
-					break;
-
-				case 'e':
-				case 'f':
-				case 'g':
-						buff += convertNumber(va_arg(p, double));
-					break;
-
-				case 's':
-						buff += va_arg(p, char *);
-					break;
-
-				case 'c':
-						buff.push_back(va_arg(p, int));
-					break;
-			}
-			ch++;
-		}
-
-		buff.push_back(*ch);
-		ch++;
-	}
-
-	out << buff;
-
-	out.close();
-}
-
-/*####################################################*/
-
 
 int ISCA_Init() 
 {
@@ -131,16 +30,26 @@ void ISCA_Quit()
 
 /*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
 
+ISCA_Win  *win;
+
+void InitForms()
+ {
+	//win = ISCA_CreateWin(startx, starty, width, height, "title", options); 
+	ISCA_RegisterWindow(win);	/* Регистрация формы окна */ 
+	ISCA_Insert(win, but);
+}
+
 int main(int argc, char const *argv[])
 {
+
 	if(ISCA_Init() < 0) {	/* Инициализация оконной системы */
 		std::cerr << 
 			"Ошибка оконной системы! Завершение работы!" << std::endl;
 		exit(1);
 	}
+
+	InitForms();			/* Регистрация рабочего окружения */
 	
-
-
 	ISCA_Quit();			/* Прекращение работы оконной системы */
 	return 0;
 }
