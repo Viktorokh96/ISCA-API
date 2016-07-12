f3ee8dd12cb1467e33f16571d99f64be25579319
Viktorokh96 <viktorokh96@mail.ru>
2016-07-12 13:43:53 +0300

    Добавлены методы, title - возможно временное поле и оно не войдёт в окончательную схему

--- testing/framework.cpp
+++ testing/framework.cpp
@@ -12,18 +12,33 @@ struct ISCA_Rect {
 	int w,h;	// Ширина и высота формы
 };
 
+typedef unsigned int opt_t;
+typedef void (*drawhandler_t)(void *ths);
+typedef void (*eventhandler_t)(void *ths, void *event);
+
 /* Стандартные поля формы:
- * chil - список потомков формы
- * curr - указатель на текущую форму (для поддержки модальности)
+ * next - слудующая форма (соседний узел в дереве), 
+ * которая принадлежит владельцу этого окна.
+ * owner - владелец текущей формы.
+ * chil - указатель на голову спискa потомков формы.
+ * curr - указатель на текущую форму (для поддержки модальности).
+ * event_handler - метод обработчика событий.
+ * draw - метод отрисовки формы.
 */
 #define STD_FORM\
+	void *next;\
+	void *owner;\
 	void *chil;\
 	void *curr;\
+	std::string title;\
 	ISCA_Rect rect;\
+	eventhandler_t event_handler;\
+	drawhandler_t draw;\
+
 
 struct ISCA_StdForm {
 	STD_FORM
-};
+} ISCA_Applic;			// Корень дерева форм приложения
 
 struct ISCA_Win {
 	STD_FORM
@@ -32,6 +47,8 @@ struct ISCA_Win {
 struct ISCA_Button {
 	STD_FORM
 };
+
+
 
 int ISCA_RegisterWindow(ISCA_Win *win)
 {
@@ -51,9 +68,29 @@ int ISCA_Assign(ISCA_Rect *rect, int x, int y, int w, int h)
 	return 0;
 }
 
+int ISCA_GetWidth() 
+{
+	return 80;		// temporary
+}
+
+int ISCA_GetHeight()
+{
+	return 25;		// temporary
+}
+
 int ISCA_Init() 
 {
 	ISCA_Log(LOGFILE, "Инициализация ISCA...\n");
+
+	ISCA_Rect rect;
+	ISCA_Assign(&rect, 0, 0 , ISCA_GetWidth(), ISCA_GetHeight());
+	
+	ISCA_Applic.owner = NULL;
+	ISCA_Applic.next = NULL;
+	ISCA_Applic.chil = NULL;
+	ISCA_Applic.curr = NULL;
+
+	ISCA_Applic.title = "Applic Main";
 
 	/* Успешная  инициализация */
 	return 0;			
@@ -61,7 +98,21 @@ int ISCA_Init()
 
 int ISCA_Insert(void* dst, void* src)
 {
-	ISCA_Log(LOGFILE, "Вставка одной формы в другую\n");
+	if(!dst || !src)
+		return -1;
+
+	ISCA_StdForm *d = (ISCA_StdForm *) dst;
+	ISCA_StdForm *s = (ISCA_StdForm *) src;
+
+	s->next = d->chil;
+	d->chil = s;
+
+	s->owner = d;
+	d->curr = s;
+
+	ISCA_Log(LOGFILE, "Теперь %s потомок %s\n",
+			s->title.c_str(), d->title.c_str());
+
 	return 0;
 }
 
@@ -85,23 +136,39 @@ void ISCA_Run()
 
 /*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
 
+ISCA_Win *ISCA_CreateWin(ISCA_Rect rect, std::string title, opt_t options)
+{
+	ISCA_Win *win = new ISCA_Win;
+	if (!win)
+		return NULL;
+	
+	win->title = title;		
+	win->rect = rect;
+	win->next = NULL;
+	win->chil = NULL;
+	win->owner = NULL;
+	win->curr = NULL;
+	win->event_handler = NULL;
+	win->draw = NULL;
+
+	return win;
+}
+
+/*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+
 ISCA_Win  *win;
-ISCA_Button *but;
 
 void InitForms()
  {
 	ISCA_Rect rect;
 
 	ISCA_Assign(&rect, 10, 10, 50, 40);	
-	//win = ISCA_CreateWin(rect, "title", options); 
+	win = ISCA_CreateWin(rect, "title", 0); 
 	
-	ISCA_Assign(&rect, 20, 20, 5, 2);
-	//but = ISCA_CreateButton(rect, "text", B_OK);
-
-	ISCA_Insert(win, but);		/* Вставка одной формы в другую */
-
 	// После постройки формы - необходима регистрация
 	ISCA_RegisterWindow(win);	/* Регистрация формы окна */ 
+
+	ISCA_Insert(&ISCA_Applic, win);
 }
 
 int main(int argc, char const *argv[])
