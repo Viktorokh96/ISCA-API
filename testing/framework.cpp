a5caa79f6eeaf1717b4e8efd4713560c75cc0291
Viktorokh96 <viktorokh96@mail.ru>
2016-07-12 22:25:21 +0300

    Добавлены функции обработки дерева форм, есть рекурсивное удаление

--- testing/framework.cpp
+++ testing/framework.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include "../iscalib/system/isca.h"
 #include "./iosys/iosystem.h"
+using std::string;
 
 #define LOGFILE "./initlog.log"
 
@@ -15,6 +16,7 @@ struct ISCA_Rect {
 typedef unsigned int opt_t;
 typedef void (*drawhandler_t)(void *ths);
 typedef void (*eventhandler_t)(void *ths, void *event);
+typedef void (*deletehandler_t)(void *ths);
 
 /* Стандартные поля формы:
  * next - слудующая форма (соседний узел в дереве), 
@@ -34,11 +36,12 @@ typedef void (*eventhandler_t)(void *ths, void *event);
 	ISCA_Rect rect;\
 	eventhandler_t event_handler;\
 	drawhandler_t draw;\
+	deletehandler_t free;\
 
 
 struct ISCA_StdForm {
 	STD_FORM
-} ISCA_Applic;			// Корень дерева форм приложения
+} *ISCA_Applic;			// Корень дерева форм приложения
 
 struct ISCA_Win {
 	STD_FORM
@@ -48,10 +51,40 @@ struct ISCA_Button {
 	STD_FORM
 };
 
-
-
-int ISCA_RegisterWindow(ISCA_Win *win)
+void ISCA_StdEventHandl(void *ths, void *event)
 {
+	return;
+}
+
+void ISCA_StdDraw(void *ths)
+{
+	return;
+}
+
+void ISCA_StdFree(void *ths)
+{
+	if(!ths) return;
+	ISCA_StdForm *frm = (ISCA_StdForm *) ths;
+
+	ISCA_Log(LOGFILE, "Освобождение формы %s\n", frm -> title.c_str());
+
+	frm -> title.~string();
+	delete ths;		// Освобождение формы
+}
+
+int ISCA_RegisterForm(void *frm, eventhandler_t evt = ISCA_StdEventHandl,
+			drawhandler_t drw = ISCA_StdDraw,
+			deletehandler_t fr = ISCA_StdFree)
+{
+	if(!frm)
+		return -1;
+
+	ISCA_StdForm *f = (ISCA_StdForm *) frm;
+
+	f->event_handler = evt;
+	f->draw = drw;
+	f->free = fr;
+	
 	return 0;
 }
 
@@ -82,15 +115,19 @@ int ISCA_Init()
 {
 	ISCA_Log(LOGFILE, "Инициализация ISCA...\n");
 
+	ISCA_Applic = new ISCA_StdForm;		// Выделение памяти под корень дерева
+
 	ISCA_Rect rect;
 	ISCA_Assign(&rect, 0, 0 , ISCA_GetWidth(), ISCA_GetHeight());
 	
-	ISCA_Applic.owner = NULL;
-	ISCA_Applic.next = NULL;
-	ISCA_Applic.chil = NULL;
-	ISCA_Applic.curr = NULL;
+	ISCA_Applic->owner = NULL;
+	ISCA_Applic->next = NULL;
+	ISCA_Applic->chil = NULL;
+	ISCA_Applic->curr = NULL;
 
-	ISCA_Applic.title = "Applic Main";
+	ISCA_Applic->free = ISCA_StdFree;
+
+	ISCA_Applic->title = "Applic Main";
 
 	/* Успешная  инициализация */
 	return 0;			
@@ -110,15 +147,33 @@ int ISCA_Insert(void* dst, void* src)
 	s->owner = d;
 	d->curr = s;
 
-	ISCA_Log(LOGFILE, "Теперь %s потомок %s\n",
+	ISCA_Log(LOGFILE, "Теперь формой %s владеет  %s\n",
 			s->title.c_str(), d->title.c_str());
 
 	return 0;
 }
 
+// Рекурсивное удаление дерева форм
+void ISCA_FreeTree(void *f)
+{
+	ISCA_StdForm *tmp, *frm = (ISCA_StdForm *) f;
+
+	ISCA_Log(LOGFILE, "Удаление дерева с вершины %s\n", frm->title.c_str());
+	while(frm->chil) {
+		tmp = (ISCA_StdForm *) frm->chil;
+		frm->chil = ((ISCA_StdForm *) (frm->chil))->next;
+		ISCA_FreeTree(tmp);
+	}
+	
+	ISCA_Log(LOGFILE, "Удаление вершины %s\n", frm->title.c_str());
+	frm->free(frm);
+}
+
 /* Статус прекращения работы не возвращается */
 void ISCA_Quit() 
 {		
+	ISCA_FreeTree(ISCA_Applic);		// Рекурсивное удаление дерева
+
 	ISCA_Log(LOGFILE, "Прекращение работы ISCA...\n");	
 }
 
@@ -150,6 +205,9 @@ ISCA_Win *ISCA_CreateWin(ISCA_Rect rect, std::string title, opt_t options)
 	win->curr = NULL;
 	win->event_handler = NULL;
 	win->draw = NULL;
+	win->free = NULL;
+
+	ISCA_Log(LOGFILE, "Создана форма %s\n", win->title.c_str());
 
 	return win;
 }
@@ -159,16 +217,16 @@ ISCA_Win *ISCA_CreateWin(ISCA_Rect rect, std::string title, opt_t options)
 ISCA_Win  *win;
 
 void InitForms()
- {
+{
 	ISCA_Rect rect;
 
 	ISCA_Assign(&rect, 10, 10, 50, 40);	
-	win = ISCA_CreateWin(rect, "title", 0); 
-	
-	// После постройки формы - необходима регистрация
-	ISCA_RegisterWindow(win);	/* Регистрация формы окна */ 
+	win = ISCA_CreateWin(rect, "first win", 0); 
 
-	ISCA_Insert(&ISCA_Applic, win);
+	// После постройки формы - необходима регистрация
+	ISCA_RegisterForm(win);	/* Регистрация формы окна */ 
+
+	ISCA_Insert(ISCA_Applic, win);
 }
 
 int main(int argc, char const *argv[])
