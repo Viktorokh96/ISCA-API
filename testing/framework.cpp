86a9aab7559ee188911add1e86d8104cd6f13af2
Viktorokh96 <viktorokh96@mail.ru>
2016-07-19 20:37:47 +0000

    fix

--- testing/framework.cpp
+++ testing/framework.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <unistd.h>
 #include <cstdlib>
-#include "../iscalib/system/isca.h"
 #include "./iosys/iosystem.h"
 using std::string;
 
@@ -231,31 +230,31 @@ ISCA_Win *ISCA_CreateWin(ISCA_Rect rect, std::string title, opt_t options)
 
 /*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
 
-ISCA_Win  *win;
 ISCA_Win  *win1;
 ISCA_Win  *win2;
 ISCA_Win  *win3;
+ISCA_Win  *win4;
 
 void InitForms()
 {
 	ISCA_Rect rect;
 
 	ISCA_Assign(&rect, 10, 10, 50, 40);	
-	win = ISCA_CreateWin(rect, "win", 0); 
+	win1 = ISCA_CreateWin(rect, "win1", 0); 
 
 	ISCA_Assign(&rect, 20, 20, 80, 50);
-	win1 = ISCA_CreateWin(rect, "win1", 0); 
+	win2 = ISCA_CreateWin(rect, "win2", 0); 
 	
 	ISCA_Assign(&rect, 20, 20, 90, 60);
-	win2 = ISCA_CreateWin(rect, "win2", 0); 
-
-	ISCA_Assign(&rect, 20, 20, 20, 10);
 	win3 = ISCA_CreateWin(rect, "win3", 0); 
 
-	ISCA_Insert(ISCA_Applic, win);
-	ISCA_Insert(win, win1);
+	ISCA_Assign(&rect, 20, 20, 20, 10);
+	win4 = ISCA_CreateWin(rect, "win4", 0); 
+
+	ISCA_Insert(ISCA_Applic, win1);
 	ISCA_Insert(win1, win2);
 	ISCA_Insert(win1, win3);
+	ISCA_Insert(win2, win4);
 }
 
 int main(int argc, char const *argv[])
